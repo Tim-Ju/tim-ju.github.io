a241c9fbd65f956401146e571a6fe6089ea84378
webartisan <webartisan@yeah.net>
2015-09-07 10:04:29 +0800

    deploy

---
+++ Gruntfile.js
@@ -0,0 +1,79 @@
+/**
+ * Created by Administrator on 2015/9/6.
+ */
+module.exports = function(grunt){
+
+    var config = {
+        "liveReloadPort": 35729,
+        "appPort":3000
+    };
+
+    require('matchdep').filterDev('grunt-*').forEach(grunt.loadNpmTasks);
+
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        watch: {
+            options: {
+                livereload: true
+            },
+            compass: {
+                files: ['assets/scss/**/*.{scss,sass}'],
+                tasks: ['compass:debug']
+            }
+        },
+        compass: {
+            debug:{
+                options: {
+                    cssDir: 'assets/css',
+                    sassDir: 'assets/scss',
+                    imagesDir: 'assets/images',
+                    relativeAssets: true,
+                    httpGeneratedImagesPath: '/images',
+                    generatedImagesDir: 'assets/images'
+                }
+            }
+        },
+        exec: {
+            build: {
+                cmd: 'jekyll build'
+            },
+            serve: {
+                cmd: 'jekyll serve --watch'
+            },
+            deploy: {
+                cmd: 'rsync --progress -a --delete -e "ssh -q" _site/ myuser@host:mydir/'
+            }
+        },
+        connect: {
+            options: {
+                port: config.appPort,
+                hostname: '0.0.0.0'
+            },
+            debug: {
+                options: {
+                    middleware: function (connect) {
+                        return [
+                            require('connect-livereload')(
+                                {port: config.liveReloadPort }
+                            ),
+                            require('_site')
+                        ];
+                    }
+                }
+            }
+        },
+
+        open: {
+            server: {
+                path: 'http://localhost:' + config.appPort
+            }
+        },
+    });
+
+    grunt.registerTask('default', [
+        'exec:build',
+        'connect:debug',
+        'open',
+        'watch'
+    ]);
+}
